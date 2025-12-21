173ea1c65ee2433384d050a6c4809fb248a2e3fc
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2025-12-21 18:46:00 +0530

    21-12-2025 primitive types done

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,6 +11,13 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(n : i32) -> i32{
+    return if n > 40 {
+        n
+    }else{
+        2*n
+    };
+}
 
 fn main() {
     // You can optionally experiment here.
